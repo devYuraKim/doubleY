fec31213fc651d73cd81ddcb4da44f4bb7772e5a
qqqkyj <123rkdduswn@naver.com>
2025-11-27 14:56:44 +0900

    finish

--- src/main/java/com/example/either/service/AnswerService.java
+++ src/main/java/com/example/either/service/AnswerService.java
@@ -1,9 +1,30 @@
 package com.example.either.service;
 
+import com.example.either.entity.Answer;
+import com.example.either.entity.Question;
+import com.example.either.repository.AnswerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
-@Transactional
+@RequiredArgsConstructor
 public class AnswerService {
+    private final AnswerRepository answerRepository;
+    private final QuestionService questionService;
+
+    public void createAnswer(Answer answer, Long questionId) {
+        Question question = questionService.getQuestionById(questionId);
+        answer.setQuestion(question);
+        answerRepository.save(answer);
+    }
+
+    public List<Answer> getAnswers(Long questionId) {
+        return answerRepository.findByQuestionId(questionId);
+    }
+
+    public Long countByAnswerTextAndQuestion(String answerText, Question question) {
+        return answerRepository.countByAnswerTextAndQuestion(answerText, question);
+    }
 }
